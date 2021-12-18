5e2ef3afd5a878e0288518ff1dfae1ef220100bf
ngoquochieu <ngoquochieu06112001@gmail.com>
2021-12-18 16:46:55 +0700

    update

--- app.js
+++ app.js
@@ -53,5 +53,5 @@ app.use((err, req, res, next) => {
 });
 
 //Start the server
-const port = app.get('port') || 3000;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
