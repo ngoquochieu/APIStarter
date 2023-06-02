ce7e990ce17d7dbd7cde7f2e7332d75e10ab460d
ngoquochieu <hieu.ngo@dssolution.vn>
2023-06-02 16:19:47 +0700

    fix code

--- config/index.js
+++ config/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-    JWT_SECRET : process.env.JWT_SECRET,
-    auth: {
-        google: {
-            CLIENT_ID : process.env.GOOGLE_CLIENT_ID,
-            CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
-        },
-        facebook: {
-            CLIENT_ID : process.env.FACEBOOK_CLIENT_ID,
-            CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET
-        },
-    }
-}
+  JWT_SECRET: process.env.JWT_SECRET,
+  auth: {
+    google: {
+      CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    facebook: {
+      CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+      CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+    },
+  },
+};
